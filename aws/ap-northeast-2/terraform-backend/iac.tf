219d3597f0886511483aa98f75218907209902d3
Jihye-Yeo <jhy2207@kico.co.kr>
2023-05-08 13:36:24 +0900

    commit

--- aws/ap-northeast-2/terraform-backend/iac.tf
+++
@@ -1,3 +0,0 @@
-module "terraform-backend" {
-  source = "git@github.com:<Github Profile명>/platform-repository.git//aws/ap-northeast-2/terraform-backend?ref=main"
-}
--- aws/ap-northeast-2/terraform-codes/iac.tf
+++
@@ -1,3 +0,0 @@
-module "terraform-codes" {
-  source = "git@github.com:<Github Profile명>/platform-repository.git//aws/ap-northeast-2/terraform-codes?ref=main"
-}
